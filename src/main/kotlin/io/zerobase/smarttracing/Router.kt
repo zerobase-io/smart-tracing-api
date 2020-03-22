3aaeb87c0775d2b70d65fdea30020c56b21df3ea
David Harris <toadzky@gmail.com>
2020-03-21 19:16:54 -0600

    Fixes mismatch between expected body formats on POST endpoints

--- src/main/kotlin/io/zerobase/smarttracing/Router.kt
+++ src/main/kotlin/io/zerobase/smarttracing/Router.kt
@@ -1,6 +1,7 @@
 package io.zerobase.smarttracing
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder
 import io.zerobase.smarttracing.models.DeviceId
 import io.zerobase.smarttracing.models.Fingerprint
 import javax.ws.rs.*
@@ -13,6 +14,9 @@ open class ApiResponse(val success: Boolean,
 data class DeviceCreatedResponse(@JsonProperty("dvid") val id: String): ApiResponse(success = true)
 data class ScanRecordedResponse(@JsonProperty("scan") val id: String): ApiResponse(success = true)
 
+data class CreateDeviceRequest(val fingerprint: String?, val ip: String?)
+data class PeerToPeerCheckIn(@JsonProperty("dvid") val scanningDevice: String, @JsonProperty("sdvid") val scannedDevice: String)
+
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -20,8 +24,8 @@ class Router(val dao: GraphDao) {
 
     @Path("/c/{unused: .+?}")
     @POST
-    fun createDevice(@FormParam("fingerprint") fingerprint: String?, @FormParam("ip") ip: String?): ApiResponse {
-        val id = dao.createDevice(fingerprint?.let{ Fingerprint(it) }, ip)
+    fun createDevice(val req: CreateDeviceRequest): ApiResponse {
+        val id = dao.createDevice(req.fingerprint?.let{ Fingerprint(it) }, req.ip)
         return when (id) {
             null -> ApiResponse(success = false, message = "Sorry, there was an error processing your account, please try again.")
             else -> DeviceCreatedResponse(id.value)
@@ -30,9 +34,8 @@ class Router(val dao: GraphDao) {
 
     @Path("/s-id/{deviceId}")
     @POST
-    fun recordCheckIn(@PathParam("deviceId") deviceId: DeviceId, @FormParam("dvid") scanningDevice: String,
-        @FormParam("sdvid") scannedDevice: String): ApiResponse {
-        val id = dao.recordPeerToPeerScan(DeviceId(scanningDevice), DeviceId(scannedDevice))
+    fun recordCheckIn(req: PeerToPeerCheckIn): ApiResponse {
+        val id = dao.recordPeerToPeerScan(DeviceId(req.scanningDevice), DeviceId(req.scannedDevice))
         return when (id) {
             null -> ApiResponse(success = false, message = "At least one provided ID is not valid.")
             else -> ScanRecordedResponse(id.value)
