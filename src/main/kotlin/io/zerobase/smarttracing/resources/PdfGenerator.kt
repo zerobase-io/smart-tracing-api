1df670b24908c38c5062867e722158b7a8d627f3
Emily <yongldich@gmail.com>
2020-04-02 03:00:04 -0700

    using kotlin's use to handle output stream while creating pdf to avoid manually handling closing an output stream etc

--- src/main/kotlin/io/zerobase/smarttracing/resources/PdfGenerator.kt
+++ src/main/kotlin/io/zerobase/smarttracing/resources/PdfGenerator.kt
@@ -26,6 +26,7 @@ import java.util.*
 class GeneratePdf {
     @Throws(Exception::class)
     fun generatePdf(businessname: String, town: String, state: String, outputfile: String) {
+        // Generate QR Code
         val genQR = GenerateQRCode(ZB_LOGO_IMAGE_PATH, QR_CODE_IMAGE_PATH)
         try {
             genQR.generateQRCodeImage(QR_VALUE, QR_WIDTH, QR_HEIGHT)
@@ -34,10 +35,11 @@ class GeneratePdf {
         } catch (e: IOException) {
             println("Could not generate QR Code, IOException :: " + e.message)
         }
+
         // We set-up a Thymeleaf rendering engine. All Thymeleaf templates
-// are HTML-based files located under "src/test/resources". Beside
-// of the main HTML file, we also have partials like a footer or
-// a header. We can re-use those partials in different documents.
+        // are HTML-based files located under "src/main/resources". Beside
+        // of the main HTML file, we also have partials like a footer or
+        // a header. We can re-use those partials in different documents.
         val templateResolver = ClassLoaderTemplateResolver()
         templateResolver.prefix = "/"
         templateResolver.suffix = ".html"
@@ -52,17 +54,17 @@ class GeneratePdf {
         dataMap["logoPath"] = QR_CODE_IMAGE_PATH
         context.setVariables(dataMap)
         // Flying Saucer needs XHTML - not just normal HTML. To make our life
-// easy, we use JTidy to convert the rendered Thymeleaf template to
-// XHTML. Note that this might not work for very complicated HTML. But
-// it's good enough for a simple letter.
+        // easy, we use JTidy to convert the rendered Thymeleaf template to
+        // XHTML. Note that this might not work for very complicated HTML. But
+        // it's good enough for a simple letter.
 
         val renderedHtmlContent = templateEngine.process("template", context)
         val xHtml = convertToXhtml(renderedHtmlContent)
         val renderer = ITextRenderer()
         // FlyingSaucer has a working directory. If you run this test, the working directory
-// will be the root folder of your project. However, all files (HTML, CSS, etc.) are
-// located under "/src/test/resources". So we want to use this folder as the working
-// directory.
+        // will be the root folder of your project. However, all files (HTML, CSS, etc.) are
+        // located under "/src/test/resources". So we want to use this folder as the working
+        // directory.
         val baseUrl = FileSystems
                 .getDefault()
                 .getPath("src", "main", "resources", "pdfconfig")
@@ -72,25 +74,21 @@ class GeneratePdf {
         print(baseUrl);
         renderer.setDocumentFromString(xHtml, baseUrl)
         renderer.layout()
+
         // And finally, we create the PDF:
         val outputStream: OutputStream = FileOutputStream(outputfile)
-        renderer.createPDF(outputStream)
-        outputStream.close()
+        outputStream.use { renderer.createPDF(it) }
     }
 
     private fun convertToXhtml(html: String): String {
-        try{
-            val tidy = Tidy()
-            tidy.inputEncoding = UTF_8
-            tidy.outputEncoding = UTF_8
-            tidy.xhtml = true
-            val inputStream = ByteArrayInputStream(html.toByteArray(StandardCharsets.UTF_8))
-            val outputStream = ByteArrayOutputStream()
-            tidy.parseDOM(inputStream, outputStream)
-            return outputStream.toString(UTF_8)
-        } catch (e: UnsupportedEncodingException){
-            throw Exception(e)
-        }
+        val tidy = Tidy()
+        tidy.inputEncoding = UTF_8
+        tidy.outputEncoding = UTF_8
+        tidy.xhtml = true
+        val inputStream = ByteArrayInputStream(html.toByteArray(StandardCharsets.UTF_8))
+        val outputStream = ByteArrayOutputStream()
+        tidy.parseDOM(inputStream, outputStream)
+        return outputStream.toString(UTF_8)
     }
 
     private fun organizationData(businessname: String, state: String, town: String): Data {
